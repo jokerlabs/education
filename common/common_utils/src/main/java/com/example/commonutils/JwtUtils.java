d1941b593d17221137463381417bd0f021467e40
220labs <xqjia@outlook.com>
2020-06-23 23:21:20 +0800

    Uncommitted changes before Checkout at 2020/06/23 10:11 [Default Changelist]

---
+++ common/common_utils/src/main/java/com/example/commonutils/JwtUtils.java
@@ -0,0 +1,84 @@
+package com.example.commonutils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+/**
+ * @author helen
+ * @since 2019/10/16
+ */
+public class JwtUtils {
+
+    public static final long EXPIRE = 1000 * 60 * 60 * 24;
+    public static final String APP_SECRET = "ukc8BDbRigUDaY6pZFfWus2jZWLPHO";
+
+    /**
+     * 生成token字符串的方法
+     */
+    public static String getJwtToken(String id, String nickname){
+
+        return Jwts.builder()
+                // 设置头信息
+                .setHeaderParam("typ", "JWT")
+                .setHeaderParam("alg", "HS256")
+
+                .setSubject("education-user")
+                // 设置token过期时间
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRE))
+
+                // 设置token的主体信息，存储用户信息
+                .claim("id", id)
+                .claim("nickname", nickname)
+
+                //
+                .signWith(SignatureAlgorithm.HS256, APP_SECRET)
+                .compact();
+    }
+
+    /**
+     * 判断token是否存在与有效
+     */
+    public static boolean checkToken(String jwtToken) {
+        if(StringUtils.isEmpty(jwtToken)) return false;
+        try {
+            Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(jwtToken);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 判断token是否存在与有效
+     */
+    public static boolean checkToken(HttpServletRequest request) {
+        try {
+            String jwtToken = request.getHeader("token");
+            if(StringUtils.isEmpty(jwtToken)) return false;
+            Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(jwtToken);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 根据token获取id
+     */
+    public static String getMemberIdByJwtToken(HttpServletRequest request) {
+        String jwtToken = request.getHeader("token");
+        if(StringUtils.isEmpty(jwtToken)) return "";
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(jwtToken);
+        Claims claims = claimsJws.getBody();
+        return (String)claims.get("id");
+    }
+}
